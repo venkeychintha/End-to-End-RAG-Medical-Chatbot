47c54ba20e2bd742a600c9661763dbcaaeada671
venkeychintha <venkateshchintha97@gmail.com>
2026-03-05 06:23:22 -0600

    created the medicl chatbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Venkatesh Chintha",
+    author_email= "venkateshchintha97@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,14 @@
+# Creating directories
+mkdir -p src
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch app.py
+touch store_index.py
+
+
+echo "Directory and files created successfully!."
